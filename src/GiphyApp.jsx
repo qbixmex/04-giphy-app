9a0f743b2ca1a6b4d0ad29c10f64244c5d5984b1
Daniel González Briseño <daniel.gb@globant.com>
2022-06-18 02:31:57 -0500

    01 - Initial Setup

---
+++ src/GiphyApp.jsx
@@ -0,0 +1,9 @@
+const key = '0B0PGiIZ9D4lOXJ2bPvE9GAXMKI7hBfc';
+
+const GiphyApp = () => {
+  return (
+    <h1>GiphyApp</h1>
+  );
+};
+
+export default GiphyApp;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GiphyApp from './GiphyApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GiphyApp />
+  </React.StrictMode>
+);
