eb6babc76b982faa98ee87b2c3a545a641dc2d33
Daniel González Briseño <daniel.gb@globant.com>
2022-06-18 02:39:47 -0500

    02 - Styles

--- src/GiphyApp.jsx
+++ src/GiphyApp.jsx
@@ -2,7 +2,13 @@ const key = '0B0PGiIZ9D4lOXJ2bPvE9GAXMKI7hBfc';
 
 const GiphyApp = () => {
   return (
+    // 1. Title
     <h1>GiphyApp</h1>
+
+    // 2. - Input
+
+    // 3. - Gifs List
+
   );
 };
 
