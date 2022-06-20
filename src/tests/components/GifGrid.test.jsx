b14ab64a2c0b44b88350feab07b256be83c7c163
Daniel González Briseño <daniel.gb@globant.com>
2022-06-20 05:56:04 -0500

    26 - Show items when images were loaded test

--- src/tests/components/GifGrid.test.jsx
+++ src/tests/components/GifGrid.test.jsx
@@ -1,10 +1,20 @@
-import { render, screen } from "@testing-library/react";
-import { GifGrid } from "../../components";
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../components';
+import useFetchGifs from '../../hooks/useFetchGifs';
+
+jest.mock('../../hooks/useFetchGifs');
 
 describe('Test ons <GifGrid />', () => {
+  const searchText = 'My Hero Academy';
+
   test('Should show title and spinner loading at beginning', () => {
-    const searchText = 'My Hero Academy';
+
     const loadingText = 'Loading...';
+
+    useFetchGifs.mockReturnValue({
+      gifs: [],
+      isLoading: true
+    });
 
     render(<GifGrid category={{ name: searchText }} />);
 
@@ -14,5 +24,34 @@ describe('Test ons <GifGrid />', () => {
 
     expect(h2.innerHTML).toBe(searchText);
     expect(loading.innerHTML).toBe(loadingText);
+  });
+
+  test('Should show items when images were loaded with useFetchGifs hook', () => {
+    const searchText = 'My Hero Academy';
+
+    useFetchGifs.mockReturnValue({
+      gifs: [
+        {
+          id: '123',
+          title: 'Dragon Ball',
+          url: 'https://localhost/dragon-ball.gif'
+        },
+        {
+          id: '345',
+          title: 'Dragon Ball Z',
+          url: 'https://localhost/dragon-ball-z.gif'
+        },
+        {
+          id: '678',
+          title: 'Dragon Ball Super',
+          url: 'https://localhost/dragon-super.gif'
+        }
+      ],
+      isLoading: false
+    });
+
+    render(<GifGrid category={{ name: searchText }} />);
+    
+    expect(screen.getAllByRole('img').length).toBe(3);
   });
 });
