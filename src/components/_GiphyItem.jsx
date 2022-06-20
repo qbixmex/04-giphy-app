599cf06c50a073b4bfa8d7a7222b81e7c2988d8f
Daniel González Briseño <daniel.gb@globant.com>
2022-06-20 03:27:53 -0500

    21 - GiphyItem tests

--- src/components/_GiphyItem.jsx
+++ src/components/_GiphyItem.jsx
@@ -4,9 +4,9 @@ const GiphyItem = ({ url, title }) => {
   return (
     <div className="col-xs-12 col-sm-6 col-lg-4">
       <div className="card mb-4">
-        <img src={url} className="card-img-top" alt={title} />
+        <img id="giphy-img" src={url} className="card-img-top" alt={title} />
         <div className="card-footer">
-          <p className="mb-0 fw-bold">{title}</p>
+          <p id="footer" className="mb-0 fw-bold">{title}</p>
         </div>
       </div>
     </div>
--- src/tests/components/GiphyItem.test.jsx
+++ src/tests/components/GiphyItem.test.jsx
@@ -7,13 +7,32 @@ describe('Test on <GiphyItem />', () => {
   const url = 'https://giphy.com/dragon-ball-super.gif';
 
   test('Should match with snapshot', () => {
+    const { container } = render(
+      <GiphyItem title={ title } url={ url } />
+    );
+    expect(container).toMatchSnapshot();
+  });
 
+  test('Should has an image with alt and url', () => {
     const { container } = render(
       <GiphyItem title={ title } url={ url } />
     );
 
-    expect(container).toMatchSnapshot();
+    const { src, alt } = container.querySelector('#giphy-img');
 
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+
+  });
+
+  test('Should has a footer title', () => {
+    const { container } = render(
+      <GiphyItem title={ title } url={ url } />
+    );
+
+    const pTitle = container.querySelector('#footer');
+
+    expect(pTitle.innerHTML).toBe(title);
   });
 
 });
